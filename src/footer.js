ba76678a4f5248c5fde6857b271063d0b7a7bd71
Jacky Lui <jackymlui@live.com>
2019-06-17 06:11:55 +1000

    layout for footer created

--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Footer = () => {
-    return (
-      <div>
-        <p>Copyright created by Jacky Lui</p>
-      </div>
-    )
-  }
+  return (
+    <Container>
+      <Row>
+        <Col xs={4}>Opening Times</Col>
+        <Col xs={4}>Address</Col>
+        <Col xs={4}>Phone Number + Booking</Col>
+      </Row>
+      <Row>
+        <Col xs={4}><p>Copyright Dragon Garden Restaurant, created by Jacky Lui</p></Col>
+        <Col xs={4}>maps</Col>
+        <Col xs={4}>facebook</Col>
+      </Row>
+    </Container>
+  );
+};
 
-  export default Footer;
+export default Footer;
+
+/*
+  <div>
+       
+      </div>*/
