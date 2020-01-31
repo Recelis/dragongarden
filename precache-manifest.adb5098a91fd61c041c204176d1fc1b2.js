205c510a103b788f2ca5750ef6e93591e545ef45
Jacky Lui <jackymlui@live.com>
2020-02-01 04:53:10 +1000

    Updates

---
+++ precache-manifest.adb5098a91fd61c041c204176d1fc1b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39c37715b47b64e35f4ece8cb8171fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85833028b77142a36914",
+    "url": "/static/css/main.f71bfa73.chunk.css"
+  },
+  {
+    "revision": "68a6794254f1b268469c",
+    "url": "/static/js/2.f9477f0f.chunk.js"
+  },
+  {
+    "revision": "85833028b77142a36914",
+    "url": "/static/js/main.caff0fdd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "571146c1c15a59492dc32c8f2c8e938d",
+    "url": "/static/media/logo.571146c1.png"
+  }
+]);
