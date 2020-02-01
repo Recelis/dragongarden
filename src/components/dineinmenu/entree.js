afeb32c4e06fe0071498b6ae72dde3e729aa9c7b
Jacky Lui <jackymlui@live.com>
2020-02-01 17:14:14 +1000

    fixed pricing

--- src/components/dineinmenu/entree.js
+++ src/components/dineinmenu/entree.js
@@ -8,8 +8,8 @@ const Entree = () => {
             <MenuItem itemNumber="1" itemName="Prawn Cocktail" itemPrice="10.50"/>
             <MenuItem itemNumber="2" itemName="Crab Cocktail" itemPrice="11.50"/>
             <MenuItem itemNumber="3" itemName="Mini Spring Rolls (4)" itemPrice="8.80"/>
-            <MenuItem itemNumber="4" itemName="King Prawn Cutlets (4)" itemPrice="14.80"/>
-            <MenuItem itemNumber="5" itemName="Coconut King Prawns (4)" itemPrice="14.80"/>
+            <MenuItem itemNumber="4" itemName="King Prawn Cutlets (4)" itemPrice="14.00"/>
+            <MenuItem itemNumber="5" itemName="Coconut King Prawns (4)" itemPrice="14.00"/>
             <MenuItem itemNumber="6" itemName="Sesame Prawn Toast (4)" itemPrice="8.00"/>
             <MenuItem itemNumber="7" itemName="Fried Scallops (6)" itemPrice="12.00"/>
             <MenuItem itemNumber="8" itemName="Curried Puffs (4)" itemPrice="8.80"/>
